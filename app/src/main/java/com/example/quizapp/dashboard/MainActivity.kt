edfc2e7ced4c6899d52ab4bea4f14c359577780c
Long <longmatic1.0@gmail.com>
2025-07-24 17:35:53 +0700

    Sửa giao diện trang chính

--- app/src/main/java/com/example/quizapp/dashboard/MainActivity.kt
+++ app/src/main/java/com/example/quizapp/dashboard/MainActivity.kt
@@ -1,4 +1,4 @@
-package com.example.quizapp
+package com.example.quizapp.dashboard
 
 import android.os.Bundle
 import androidx.activity.ComponentActivity
---
+++ app/src/main/java/com/example/quizapp/dashboard/MainScreen.kt
@@ -0,0 +1,10 @@
+package com.example.quizapp.dashboard
+
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.tooling.preview.Preview
+
+@Composable
+@Preview
+fun MainScreen() {
+
+}
